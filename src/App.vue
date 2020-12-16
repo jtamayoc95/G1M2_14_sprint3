9e54d0160dcb8e6230e9a8f790a0d56f3c1b5ec3
elver <contact@fastweb.com>
2020-12-15 19:15:32 -0500

    Siguiendo pasos diapositiva 14

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div id="app">
     <div class="header">
   
-      <h1>Banco UN</h1>
+      <h1>Midas</h1>
       <nav>
         <button v-on:click="init" v-if="is_auth" > Inicio </button>
-        <button v-on:click="getBalance" v-if="is_auth" > Saldo </button>
-        <button v-if="is_auth" > Transacción </button>
+        <button v-on:click="getInfo" v-if="is_auth" > Información </button>
+        <button v-if="is_auth" > Cambiar contraseña </button>
         <button v-if="is_auth" >Cerrar Sesión</button>
       </nav>
     </div>
 
     <div class="main-component">
-
+      <router-view></router-view>
     </div>
     
     <div class="footer">
-      <h2>Misión TIC 2022</h2>
+      <h2>G1M2_14</h2>
     </div>
 
   </div>
@@ -35,12 +35,26 @@ export default {
   },
   
   methods: {
+    init: function(){
+      if(this.$route.name != "user"){
+        let username = localStorage.getItem("current_username")
+        this.$router.push({name: "user", params:{username:username}})
+      }
+    },
+  },
 
+  getInfo: function(){
+    if(this.$route.name != "user_info"){
+      let username = localStorage.getItem("current_username")
+      this.$router.push({ name:"user_info", params:{username:username}})
+    }
   },
 
   beforeCreate: function(){
-    localStorage.setItem('current_username', 'camilo24')
+    localStorage.setItem('current_username', 'cabe')
     localStorage.setItem('isAuth', true)
+
+    this.$router.push({name:"user",params:{username:'cabe'}})
   }
 }
 </script>
@@ -55,8 +69,8 @@ export default {
     width: 100%;
     height: 10vh;
     min-height: 100px;
-    background-color: #283747 ;
-    color:#E5E7E9 ;
+    background-color: #dbd038 ;
+    color:#000000 ;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -64,6 +78,9 @@ export default {
   .header h1{
     width: 20%;
     text-align: center;
+    font-size: 40px;
+    font-weight: bolder;
+    font-style: italic;
   }
   .header nav {
     height: 100%;
@@ -72,18 +89,19 @@ export default {
     justify-content: space-around;
     align-items: center;
     font-size: 20px;
+    font-family: sans-serif;
   }
   .header nav button{
-    color: #E5E7E9;
-    background: #283747;
-    border: 1px solid #E5E7E9;
+    color: #000000;
+    background: #dbd038;
+    border: 1px solid #000000;
     border-radius: 5px;
     padding: 10px 20px;
   }
   .header nav button:hover{
-    color: #283747;
-    background: #E5E7E9;
-    border: 1px solid #E5E7E9;
+    color: #ffffff;
+    background: #99922a;
+    border: 1px solid #000000;
   }
   .main-component{
     height: 75vh;
@@ -97,8 +115,8 @@ export default {
     width: 100%;
     height: 10vh;
     min-height: 100px;
-    background-color: #283747;
-    color: #E5E7E9;
+    background-color: #dbd038;
+    color: #000000;
   }
   .footer h2{
     width: 100%;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+import vueRouter from 'vue-router'
+
+import router from './router'
+Vue.use(vueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app', 
-  components: { App }, 
+  router,
+  el: '#app',
+  components: { App },
   template: '<App/>'
 })
+
